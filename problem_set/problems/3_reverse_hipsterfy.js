0dbd6d7d2da583cf0a3e9b3e68b31bda11b760da
cfthorne <cfthorne83@gmail.com>
2022-06-16 19:12:38 -0400

    reverse

--- problem_set/problems/3_reverse_hipsterfy.js
+++ problem_set/problems/3_reverse_hipsterfy.js
@@ -11,10 +11,45 @@ reverseHipsterfy("towel flicker banana"); // => 'twel flcker bnna'
 reverseHipsterfy("runner anaconda"); // => 'rnner ncnda'
 reverseHipsterfy("turtle cheeseburger fries"); // => 'trtle chsbrger fres'
 ***********************************************************************/
+const findVowelIndex = word => {
+    let vowels = 'aeiou';
+
+    for (let i = word.length - 1; i >= 0; i--){
+        if (vowels.includes(word[i])){
+            return i;
+        }
+    }
+};
+
 
 function reverseHipsterfy(sentence) {
+    let vowels = 'aeiou';
+    let newSent = sentence.split(' ');
+    let newArr = [];
+    let str = '';
+
+    newSent.forEach(ele => {
+       let vowelIndex = findVowelIndex(ele);
+
+        for (let i = 0; i < ele.length; i++){
+            if( i === vowelIndex){
+                str += ele[i];
+            }
+
+            if (!vowels.includes(ele[i])){
+                str += ele[i];
+            }
+        }
+
+        newArr.push(str);
+        str = '';
+    })
+    
+    return newArr.join(' ');
 
 }
+
+console.log(reverseHipsterfy("proper tonic panther"))
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = reverseHipsterfy;
